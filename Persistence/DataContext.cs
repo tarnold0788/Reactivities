c7ed394efde90119719b69a723c1422bbbd21f57
TA <tarnold0788@gmail.com>
2020-04-14 09:26:14 -0500

    End of section 4 again

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Domain;
+﻿using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
@@ -13,15 +12,15 @@ namespace Persistence
         public DbSet<Value> Values { get; set; }
 
         public DbSet<Activity> Activites {get; set;}
-
+  
         protected override void OnModelCreating(ModelBuilder builder) 
         {
             builder.Entity<Value>()
              .HasData(
                  new Value {Id=1, Name = "Value101"},
-                 new Value {Id=2, Name = "Value102"},
-                 new Value {Id=3, Name = "Value103"}
-             );
+                 new Value { Id = 2, Name = "Value102" },
+                 new Value { Id = 3, Name = "Value103" }
+             ); Id =  
         }
     }
 }
