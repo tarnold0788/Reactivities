844f86bf61565afe3c3e77186cbe746b2d037597
TA <tarnold0788@gmail.com>
2020-04-14 09:41:47 -0500

    Section four clerical fixes

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,21 +22,21 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(opt => 
+            services.AddDbContext<DataContext>(opt =>
             {
-               opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
-            {
-             services.AddMediatR(typeof(List.Handler).Assembly);
+            });
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddControllers();
         }
-           
 
-               
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -6,21 +6,21 @@ namespace Persistence
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) : base(options)
-        {  
+        {
         }
 
         public DbSet<Value> Values { get; set; }
 
-        public DbSet<Activity> Activites {get; set;}
-  
-        protected override void OnModelCreating(ModelBuilder builder) 
+        public DbSet<Activity> Activites { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Value>()
              .HasData(
-                 new Value {Id=1, Name = "Value101"},
+                 new Value { Id = 1, Name = "Value101" },
                  new Value { Id = 2, Name = "Value102" },
                  new Value { Id = 3, Name = "Value103" }
-             ); Id =  
+             );
         }
     }
 }
